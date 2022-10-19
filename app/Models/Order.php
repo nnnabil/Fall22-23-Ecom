356167dc51f6a20ed32309d1cff1682f1730ba16
unknown <rhnabil.aiub@gmail.com>
2022-10-19 09:22:00 +0600

    3. add product, file upload, association

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -9,4 +9,13 @@ class Order extends Model
 {
     use HasFactory;
     public $timestamps   = false;
+
+    
+    public function orderdetails(){
+        return $this->hasMany(OrderDetail::class,'order_id');
+        //general syntax hasMany(model, foreignkey,primarykey)
+    }
+    public function customer(){
+        return $this->belongsTo(Customer::class,'customer_id','phone');
+    }
 }
